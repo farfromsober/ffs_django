318acb4397804954fbf11728ad90c8d1c117405b
Julio Martínez <juliomb89@gmail.com>
2015-12-11 12:27:31 +0000

    Creamos a mano serializer para actualizar objeto Profile, sin usar ModelSerializers.

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'gloria'
+from django.contrib.auth.hashers import make_password
 
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.relations import PrimaryKeyRelatedField
 from users.models import Profile
+from users.settings import STATE_MAX_LENGTH, CITY_MAX_LENGTH
+
 
 class StringToFloatField(serializers.Field):
 
@@ -47,14 +49,104 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 
-class ProfileUpdateSerializer(ProfileSerializer):
-
-    user = PrimaryKeyRelatedField(queryset=User.objects.all())
-
-    class Meta(ProfileSerializer.Meta):
-        fields=('user','avatar','latitude','longitude','city','state','sales')
 
 class ProfileListSerializer(ProfileSerializer):
     user = UserListSerializer()
     class Meta (ProfileSerializer.Meta):
         fields=('user','avatar','latitude','longitude','city','state','sales')
+
+
+
+
+
+
+
+class UserUpdateSerializer(serializers.Serializer):
+
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField(allow_blank=True, allow_null=True)
+    password = serializers.CharField(required=False)
+
+    def update(self, instance, validated_data):
+
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+
+
+
+
+
+
+class ProfileUpdateSerializer(serializers.Serializer):
+
+    avatar = serializers.URLField(allow_null=True, allow_blank=True)
+    latitude = serializers.CharField(max_length=50)
+    longitude = serializers.CharField(max_length=50)
+    city = serializers.CharField(max_length=CITY_MAX_LENGTH, allow_null=True, allow_blank=True)
+    state = serializers.CharField(max_length=STATE_MAX_LENGTH, allow_null=True, allow_blank=True)
+    sales = serializers.IntegerField(min_value=0)
+    user = UserUpdateSerializer()
+
+
+    def update(self, instance, validated_data):
+
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+
+        # latitud y longitud hay que pasarlos a float
+        instance.latitude = float(validated_data.get('latitude', instance.latitude))
+        instance.longitude = float(validated_data.get('longitude', instance.longitude))
+
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+        instance.sales = validated_data.get('sales', instance.sales)
+
+        # actualizamos el usuario
+        user_dict = validated_data.get('user', None)
+        if user_dict is not None:
+            instance.user.first_name = user_dict.get('first_name', instance.user.first_name)
+            instance.user.last_name = user_dict.get('last_name', instance.user.last_name)
+            instance.user.email = user_dict.get('email', instance.user.email)
+
+            # solo actualizamos el password en caso de que nos venga
+            new_password = user_dict.get('password', None)
+            if new_password is not None:
+                instance.user.password = make_password(new_password)
+
+        instance.save()
+        instance.user.save()
+        return instance
+
+
+    def validate_sales(self, value):
+        if value < 0:
+            raise serializers.ValidationError(u'Las ventas no pueden ser menores que cero')
+        return value
+
+
+
+    def validate_latitude(self, value):
+        try:
+            float(value)
+        except:
+            raise serializers.ValidationError(u'El formato de la latitud introducida no es válido')
+        return value
+
+
+    def validate_longitude(self, value):
+        try:
+            float(value)
+        except:
+            raise serializers.ValidationError(u'El formato de la longitud introducida no es válido')
+        return value
+
+
+
+
+
+
+
+
+
