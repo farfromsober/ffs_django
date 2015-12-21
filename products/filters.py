ec4bd5e537fc00f6e868637ddf9748e5cd705eea
juliomb <juliomb89@gmail.com>
2015-12-21 13:22:07 +0100

    Añadimos filtro por nombre de producto

--- products/filters.py
+++ products/filters.py
@@ -6,7 +6,8 @@ from .models import Product
 class ProductsFilter(django_filters.FilterSet):
 
     category = django_filters.NumberFilter(name="category__index")
+    name = django_filters.CharFilter(name='name', lookup_type='contains')
 
     class Meta:
         model = Product
-        fields = ('category',)
+        fields = ('category', 'name')
