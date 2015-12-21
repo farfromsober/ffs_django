e36552d49c7c38bafa8b699094fd7fe619bc8258
juliomb <juliomb89@gmail.com>
2015-12-21 13:27:35 +0100

    Añadimos filtrado por vendedor

--- products/filters.py
+++ products/filters.py
@@ -6,8 +6,9 @@ from .models import Product
 class ProductsFilter(django_filters.FilterSet):
 
     category = django_filters.NumberFilter(name="category__index")
-    name = django_filters.CharFilter(name='name', lookup_type='contains')
+    name = django_filters.CharFilter(name='name', lookup_type='icontains')
+    seller = django_filters.CharFilter(name='seller__user__username')
 
     class Meta:
         model = Product
-        fields = ('category', 'name')
+        fields = ('category', 'name', 'seller')
