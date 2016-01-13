72ba28bf439c5ea566280873bd0d6f96b82dadd0
David Regatos <david.regatos@gmail.com>
2016-01-13 23:53:48 +0100

    added more images.tests;

--- images/tests.py
+++ images/tests.py
@@ -4,10 +4,12 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from users.models import Profile
+from products.models import Product
+from images.models import Image
 from products.models import Product, Category
 
 # Create your tests here.
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 
 
 class ImageAPITestCase(APITestCase):
@@ -17,7 +19,10 @@ class ImageAPITestCase(APITestCase):
     username2 = 'testuser2'
     password2 = 'testuser2'
 
+    # Setup each test
     def setUp(self):
+
+        self.client = APIClient()
 
         # Se crean dos usuarios para asociarlos a dos perfiles
         self.user1 = User.objects.create_user(username='testuser1', password='testuser1',
@@ -62,37 +67,118 @@ class ImageAPITestCase(APITestCase):
         self.product1.save()
         self.product2.save()
 
-    def _require_login(self, username, password):
-        self.client.login(username=username, password=password)
+    # Helper to login
+    def _require_login(self, user):
+        self.client.force_authenticate(user)
+
+    ## CREATE tests ##
 
     def test_add_new_image_without_authentication(self):
         """
-        Prueba que se devuelve un 'Forbidden' si se intenta añadir imágenes
+        Prueba que se devuelve un 'Unauthorized' si se intenta añadir imágenes
         a un producto sin que el usuario esté autenticado
         """
         post_data = {
             "productId": 1,
             "urls": [
-                "http://www.image1.com",
-                "http://www.image2.com"
+                "http://www.image1.com"
             ]
         }
         response = self.client.post('/api/1.0/images/', data=post_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_add_new_image_status_code_created(self):
+        """
+        Prueba que se devuelve un 'Unauthorized' si se intenta añadir imágenes
+        a un producto sin que el usuario esté autenticado
+        """
+        self._require_login(self.user1)
+
+        post_data = {
+            "productId": 1,
+            "urls": [
+                "http://www.image1.com"
+            ]
+        }
+        response = self.client.post('/api/1.0/images/', data=post_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_add_new_image_storage(self):
+
+        self._require_login(self.user1)
+
+        post_data = {
+            "productId": 1,
+            "urls": [
+                "http://www.image1.com"
+            ]
+        }
+        self.client.post('/api/1.0/images/', data=post_data, format='json')
+
+        product1 = Product.objects.get(pk=1)
+        image1 = Image.objects.get(product = product1)
+        self.assertEqual(image1.url, "http://www.image1.com")
+
+    ## RETRIEVE test ##
 
     def test_get_image_without_authentication(self):
         """
-        Prueba que se devuelve un 'Forbidden' si el usuario intenta
+        Prueba que se devuelve un 'Unauthorized' si el usuario intenta
         recuperar las imágenes de un producto sin estar autenticado
         """
         response = self.client.get('/api/1.0/images/1/')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_image_of_not_existing_product(self):
         """
-        Prueba que no se devuelve un 'Not found' si no
+        Prueba que se devuelve un 'Not found' si no
         existe ningún producto con ese productId
         """
-        self._require_login(self.username, self.password)
+        self._require_login(self.user1)
         response = self.client.get('/api/1.0/images/55/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_image_of_product(self):
+        """
+        Prueba que se devuelve un array de urls
+        asociado al producto
+        """
+        self._require_login(self.user1)
+
+        product1 = Product.objects.get(pk=1)
+        image1 = Image.objects.create(product=product1, url="http://www.image1.com")
+
+        response = self.client.get('/api/1.0/images/1')
+        self.assertEqual(response.data, image1.url)
+
+    ## UPDATE test ##
+
+    def test_update_image_without_authentication(self):
+        """
+        Prueba que se devuelve un 'Unauthorized' si el usuario intenta
+        recuperar las imágenes de un producto sin estar autenticado
+        """
+        response = self.client.put('/api/1.0/images/1/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    ## DESTROY test ##
+
+    def test_destroy_image_not_allowed(self):
+        """
+        Prueba que no se devuelve un 'Not allowed' si
+        el usuario intenta hacer un DELETE de un producto
+        """
+        self._require_login(self.user1)
+        response = self.client.delete('/api/1.0/images/1/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    ## LIST test ##
+
+    def test_list_image_not_allowed(self):
+        """
+        Prueba que no se devuelve un 'Not allowed' si
+        el usuario intenta hacer un DELETE de un producto
+        """
+        self._require_login(self.user1)
+        response = self.client.get('/api/1.0/images/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
